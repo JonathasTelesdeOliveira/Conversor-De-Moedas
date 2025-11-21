1139028087c26aa9cfe058a1eca1a9bc2e415c52
JonathasTelesdeOliviera <jonathasteles777@gmail.com>
2025-11-21 02:11:37 -0300

    Foi Criado até aqui um consutador de moedas

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,50 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        ConsultarMoeda consultarMoeda = new ConsultarMoeda();
 
+        while (true) {
+            System.out.println("Digite o código da MOEDA: (ou 'sair' para encerrar): ");
+            String base_code = entrada.nextLine().trim();
+
+            if (base_code.equalsIgnoreCase("sair")){
+                System.out.println("Encerramento pipeline de execução.");
+                break;
+            }
+
+            // Validação de campo crítico
+            if (!base_code.matches("^[A-Za-z]{3}$")) {
+                System.out.println("Código de moeda inválido. Use o padrão ISO-4217 (ex: USD, BRL, EUR).");
+                continue;
+            }
+
+            System.out.println("""
+                    Escolha a operação:
+                    1 - Consultar Moeda");
+                    2 - Conversão (indisponível)
+                    """);
+                int num;
+                try {
+                    num = Integer.parseInt(entrada.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Entrada inválida!!!");
+                    continue;
+                }
+                switch (num) {
+                    case 1:
+                        Moeda moeda =
+                                consultarMoeda.ConsultarMoeda(base_code);
+                        System.out.println(moeda);
+                    break;
+                    case 2:
+                        System.out.println("Opão fora do ar");
+                    break;
+                    default:
+                        throw new IllegalStateException("Invalid day: " + base_code);
+                }
+        }
     }
 }
---
+++ src/Moeda.java
@@ -0,0 +1,4 @@
+public record Moeda(String base_code,
+                    String time_next_update_utc,
+                    String time_last_update_utc ) {
+}
