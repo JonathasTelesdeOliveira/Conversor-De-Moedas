764a9fa8204a55c10abf4eebd46eb2c7636e48c0
JonathasTelesdeOliviera <jonathasteles777@gmail.com>
2025-11-22 19:13:03 -0300

    Adicionando Badge-Conversor no READAME.md

--- src/GeradorArquivo.java
+++ src/GeradorArquivo.java
@@ -5,11 +5,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class GeradorArquivo {
-    public void salvaJson(Moeda moeda) throws IOException {
+    public void salvaJson(Object object, String nomeArquivo) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-        FileWriter escrita = new FileWriter(moeda.base_code()+".json");
-        escrita.write(gson.toJson(moeda));
+        FileWriter escrita = new FileWriter(nomeArquivo+".json");
+        escrita.write(gson.toJson(object));
         escrita.close();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,59 +4,83 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Bem vindo ao conversor de MOEDAS!!!!!!!!!!!\n");
-        System.out.println("Convert USD para BRL");
+        System.out.println("=== Conversor de Moedas ===");
+        System.out.println("Bem-vindo ao conversor de MOEDAS!!!!!!!!!!!\n");
 
-        System.out.println("Digite o valor da Conversão: ");
-        Double valor = entrada.nextDouble();
+        String opcao = "";
+        while (!opcao.equalsIgnoreCase("sair")) {
+            System.out.println("Painel de opções:");
+            System.out.println("1 - Converter moedas");
+            System.out.println("Digite 'sair' para encerrar");
+            System.out.print("Escolha: ");
+            opcao = entrada.nextLine().trim();
 
-        entrada.nextLine();
-        System.out.println("Moeda De Origem: ");
-        System.out.println("De: ");
-        String base_code = entrada.nextLine().trim();
-
-        System.out.println("############################################");
-        System.out.println("Moeda De Destino: ");
-        System.out.println("Para: ");
-        String target_code = entrada.nextLine().trim();
-
-        if (base_code.equalsIgnoreCase("sair") | target_code.equalsIgnoreCase("sair")) {
-            System.out.println("Encerramento pipeline de execução." + base_code + " , " + target_code);
-            return;
-        }
-        // Validação
-        if (!base_code.matches("^[A-Za-z]{3}$") | !target_code.matches("^[A-Za-z]{3}$")) {
-            System.out.println("Código de moeda inválido. Use o padrão ISO-4217 (ex: USD, BRL, EUR)." + base_code + " , " + target_code);
-        }
-        try {
-            /*      *************************************************           */
-
-            ConsultarMoedaManualmente consultarMoedas = new ConsultarMoedaManualmente();
-            Moeda moeda = consultarMoedas.ConsultarMoeda(base_code);
-
-            GeradorArquivo geradorArquivo = new GeradorArquivo();
-            geradorArquivo.salvaJson(moeda);
-
-            //converter
-            Map<String, Double> taxas = moeda.listaMap();
-            double resultado = consultarMoedas.Converter(valor, base_code, target_code, taxas);
-
-            // CONVERSÃO AUTOMÁTICA
-            ConsultarMoedaAutomaticamente consultarMoedaAutomaticamente = new ConsultarMoedaAutomaticamente();
-            MoedaPair moedaPair = consultarMoedaAutomaticamente.ConsultarMoedaAutomaticamente(base_code, target_code);
-            if (moedaPair == null) {
-                System.out.println("Falha ao consultar API. Pipeline encerrado.");
-                return;
+            if (opcao.equalsIgnoreCase("sair")) {
+                System.out.println("Encerrando o programa...");
+                break;
             }
-            System.out.println("\n_______________________________________________________________________________________\n");
-            System.out.printf("Moeda Base: %s %.2f", base_code, valor);
-            System.out.printf("\nMoeda Destino: %s%n", target_code);
-            System.out.printf("Taxa de conversão: %.4f%n", moedaPair.conversion_rate());
-            System.out.printf("Conversão realizada: %.2f %s = %.2f %s%n",
-                    valor, base_code, resultado, target_code);
-            System.out.println("\n_______________________________________________________________________________________\n");
-        } catch (Exception e) {
-            System.out.println("Errror::::" + e.getMessage());
+            if (opcao.equals("1")) {
+                System.out.println("Digite o valor da Conversão: ");
+                Double valor = entrada.nextDouble();
+                entrada.nextLine(); // consumir quebra de linha
+
+                System.out.println("Moeda De Origem (ex: USD, BRL, ARS): ");
+                String base_code = entrada.nextLine().trim();
+
+                System.out.println("Moeda De Destino (ex: USD, BRL, ARS): ");
+                String target_code = entrada.nextLine().trim();
+
+                if (base_code.equalsIgnoreCase("sair") | target_code.equalsIgnoreCase("sair")) {
+                    System.out.println("Encerramento pipeline de execução." + base_code + " , " + target_code);
+                    return;
+                }
+                // Validação
+                if (!base_code.matches("^[A-Za-z]{3}$") | !target_code.matches("^[A-Za-z]{3}$")) {
+                    System.out.println("Código de moeda inválido. Use o padrão ISO-4217 (ex: USD, BRL, EUR)." + base_code + " , " + target_code);
+                }
+
+                try {
+                   ConsultarMoedaManualmente consultarMoedas = new ConsultarMoedaManualmente();
+                    Moeda moeda = consultarMoedas.ConsultarMoeda(base_code);
+
+                    GeradorArquivo geradorArquivo = new GeradorArquivo();
+                    geradorArquivo.salvaJson(moeda,"resultado_"+ base_code + "para_"+ target_code);
+
+                    //converter
+                    Map<String, Double> taxas = moeda.listaMap();
+                    double resultado = consultarMoedas.Converter(valor, base_code, target_code, taxas);
+
+                    // CONVERSÃO AUTOMÁTICA
+                    ConsultarMoedaAutomaticamente consultarMoedaAutomaticamente = new ConsultarMoedaAutomaticamente();
+                    MoedaPair moedaPair = consultarMoedaAutomaticamente.ConsultarMoedaAutomaticamente(base_code, target_code);
+
+                    ResultadoDaConversao resultadoDaConversao = new ResultadoDaConversao(
+                            base_code,
+                            target_code,
+                            valor,
+                            moedaPair.conversion_rate(),
+                            resultado);
+                    GeradorArquivo geradorArquivoConversao = new GeradorArquivo();
+                    geradorArquivoConversao.salvaJson(resultadoDaConversao, "resultado_"+ base_code + "para_"+ target_code);
+
+                    if (moedaPair == null) {
+                        System.out.println("Falha ao consultar API. Pipeline encerrado.");
+                        return;
+                    }
+                    System.out.println("\n_______________________________________________________________________________________\n");
+                    System.out.printf("Moeda Base: %s %.2f", base_code, valor);
+                    System.out.printf("\nMoeda Destino: %s%n", target_code);
+                    System.out.printf("Taxa de conversão: %.4f%n", moedaPair.conversion_rate());
+                    System.out.printf("Conversão realizada: %.2f %s = %.2f %s%n",
+                            valor, base_code, resultado, target_code);
+                    System.out.println("\n_______________________________________________________________________________________\n");
+                } catch (Exception e) {
+                    System.out.println("Errror::::" + e.getMessage());
+                }
+            } else {
+                System.out.println("Opção inválida, tente novamente.");
+            }
         }
+        entrada.close();
     }
 }
